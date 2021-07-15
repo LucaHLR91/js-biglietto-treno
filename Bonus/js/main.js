d549f864c448817815ba9849f7403cad8be04ff5
Luca <mastrantonio.luca.91@icloud.com>
2021-07-15 16:23:02 +0200

    Bonus code optimization

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -4,11 +4,11 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 
 il prezzo del biglietto è definito in base ai km (0.21 € al km) - done
 
-va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 20% per i minorenni - done
 
-va applicato uno sconto del 40% per gli over 65.
+va applicato uno sconto del 40% per gli over 65. -done
 
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. -done
 
 BONUS: introduzione della parte grafica con CSS */
 
@@ -21,18 +21,18 @@ var chilometri = prompt('inserisci i km che percorri');
 var prezzo_km = 0.21;
 var prezzo_viaggio = prezzo_km * chilometri;
 
-// prezzo ridotto u18
-var sconto_u18 = (prezzo_viaggio * 20) / 100;
-var prezzo_u18 = prezzo_viaggio - sconto_u18;
+// // prezzo ridotto u18
+// var sconto_u18 = (prezzo_viaggio * 20) / 100;
+// var prezzo_u18 = prezzo_viaggio - sconto_u18;
 
-// prezzo ridotto over65
-var sconto_over65 = (prezzo_viaggio * 40) / 100;
-var prezzo_over65 = prezzo_viaggio - sconto_over65;
+// // prezzo ridotto over65
+// var sconto_over65 = (prezzo_viaggio * 40) / 100;
+// var prezzo_over65 = prezzo_viaggio - sconto_over65;
 
 if (eta < 18) {
-    console.log('il prezzo del tuo biglietto è ' + prezzo_u18.toFixed(2) + '€')
+    console.log('il prezzo del tuo biglietto è ' + (prezzo_viaggio - ((prezzo_viaggio * 20) / 100)).toFixed(2) + '€')
 }else if (eta > 65) {
-    console.log('il prezzo del tuo biglietto è ' + prezzo_over65.toFixed(2) + '€')
+    console.log('il prezzo del tuo biglietto è ' + (prezzo_viaggio - ((prezzo_viaggio * 40) / 100)).toFixed(2) + '€')
 }else {
     console.log('il prezzo del tuo biglietto è ' + prezzo_viaggio.toFixed(2) + '€')
 }
